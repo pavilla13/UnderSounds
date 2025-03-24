b557d1d36a0ae95e2a139f9d4d5ca61a75895793
JAVIERCOSTILLOPALACIOS <124663264+JAVIERCOSTILLOPALACIOS@users.noreply.github.com>
2025-03-24 14:43:04 +0100

    Create albumes.js

---
+++ js/albumes.js
@@ -0,0 +1,28 @@
+export const albumes = [
+  { name: "Álbum 1", link: "albumes.html?id=11" },
+  { name: "Álbum 2", link: "albumes.html?id=21" },
+  { name: "Álbum 3", link: "albumes.html?id=31" },
+  { name: "Álbum 4", link: "albumes.html?id=41" },
+  { name: "Álbum 5", link: "albumes.html?id=51" },
+  { name: "Álbum 6", link: "albumes.html?id=61" },
+  { name: "Álbum 7", link: "albumes.html?id=71" },
+  { name: "Álbum 8", link: "albumes.html?id=81" },
+  { name: "Álbum 9", link: "albumes.html?id=91" },
+  { name: "Álbum 10", link: "albumes.html?id=110" },
+  // Añade aquí más álbumes según sea necesario
+  // albumes.js
+
+  { name: "A Night at the Opera", link: "album.html?id=1", imgSrc: "assets/images/opera.jpeg" },
+  { name: "Future Nostalgia", link: "album.html?id=2", imgSrc: "assets/images/futuro.jpeg" },
+  { name: "Discovery", link: "album.html?id=3", imgSrc: "assets/images/discovery.jpeg" },
+  { name: "Kind of Blue", link: "album.html?id=4", imgSrc: "assets/images/blue.jpeg" }
+
+
+
+  { name: "Led Zeppelin IV", link: "album.html?id=9", imgSrc: "assets/images/led_IV.jpg" },
+  { name: "Thriller", link: "album.html?id=10", imgSrc: "assets/images/thriller.jpeg" },
+  { name: "Random Access Memories", link: "album.html?id=11", imgSrc: "assets/images/ram.jpeg" },
+  { name: "Blue Train", link: "album.html?id=12", imgSrc: "assets/images/blue_train.jpg" }
+
+
+];
