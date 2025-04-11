8fa51aa2cc95f6e4ac11d82536c670ea6c7837ad
Jose Manuel <jpecerob@alumnos.unex.es>
2025-04-11 23:15:55 +0200

    vistas añadidas

--- controller/controller.py
+++ controller/controller.py
@@ -16,26 +16,79 @@ def index(request: Request):
     #return {"message": "Hello World"}
     return view.get_index_view(request)
 
-@app.get("/tienda")
-def tienda(request: Request):
-    return view.get_tienda_view(request)
+@app.get("/album")
+def album(request: Request):
+    return view.get_album_view(request)
+
+@app.get("/amigoLista")
+def amigoLista(request: Request):
+    return view.get_amigoLista_view(request)
+
+@app.get("/amigos")
+def amigos(request: Request):
+    return view.get_amigos_view(request)
 
 @app.get("/ayuda")
 def ayuda(request: Request):
     return view.get_ayuda_view(request)
 
-@app.get("/terminos")
-def terminos(request: Request):
-    return view.get_terminos_view(request)
+@app.get("/cancion")
+def cancion(request: Request):
+    return view.get_cancion_view(request)
+
+@app.get("/checkout")
+def checkout(request: Request):
+    return view.get_checkout_view(request)
+
+@app.get("/edit_perfil")
+def edit_perfil(request: Request):
+    return view.get_edit_perfil_view(request)
+
+@app.get("/favoritos")
+def favoritos(request: Request):
+    return view.get_favoritos_view(request)
+
+@app.get("/generos_especifico")
+def generos_especifico(request: Request):
+    return view.get_generos_especifico_view(request)
+
+@app.get("/generos")
+def generos(request: Request):
+    return view.get_generos_view(request)
 
 @app.get("/login")
 def login(request: Request):
     return view.get_login_view(request)
 
-@app.get("/register")
-def register(request: Request):
-    return view.get_register_view(request)
+@app.get("/mis_compras")
+def mis_compras(request: Request):
+    return view.get_mis_compras_view(request)
+
+@app.get("/producto")
+def producto(request: Request):
+    return view.get_producto_view(request)
 
 @app.get("/recover")
 def recover(request: Request):
     return view.get_recover_view(request)
+
+@app.get("/register")
+def register(request: Request):
+    return view.get_register_view(request)
+
+@app.get("/resumen_compra")
+def resumen_compra(request: Request):
+    return view.get_resumen_compra_view(request)
+
+@app.get("/search")
+def search(request: Request):
+    return view.get_search_view(request)
+
+@app.get("/terminos")
+def terminos(request: Request):
+    return view.get_terminos_view(request)
+
+@app.get("/tienda")
+def tienda(request: Request):
+    return view.get_tienda_view(request)
+
--- view/view.py
+++ view/view.py
@@ -14,20 +14,60 @@ class View():
     def get_index_view(self, request: Request):
         return templates.TemplateResponse("index.html", {"request" : request})
     
-    def get_tienda_view(self, request: Request):
-        return templates.TemplateResponse("tienda.html", {"request" : request})
-
+    def get_album_view(self, request: Request):
+        return templates.TemplateResponse("album.html", {"request" : request})
+    
+    def get_amigoLista_view(self, request: Request):
+        return templates.TemplateResponse("amigoLista.html", {"request" : request})
+    
+    def get_amigos_view(self, request: Request):
+        return templates.TemplateResponse("amigos.html", {"request" : request})
+    
     def get_ayuda_view(self, request: Request):
         return templates.TemplateResponse("ayuda.html", {"request" : request})
+    
+    def get_cancion_view(self, request: Request):
+        return templates.TemplateResponse("cancion.html", {"request" : request})
+    
+    def get_checkout_view(self, request: Request):
+        return templates.TemplateResponse("checkout.html", {"request" : request})
+    
+    def get_edit_perfil_view(self, request: Request):
+        return templates.TemplateResponse("edit_perfil.html", {"request" : request})
+    
+    def get_favoritos_view(self, request: Request):
+        return templates.TemplateResponse("favoritos.html", {"request" : request})
+    
+    def get_generos_especifico_view(self, request: Request):
+        return templates.TemplateResponse("generos especifico.html", {"request" : request})
+    
+    def get_generos_view(self, request: Request):
+        return templates.TemplateResponse("generos.html", {"request" : request}) 
+
+    def get_login_view(self, request: Request):
+        return templates.TemplateResponse("login.html", {"request" : request})
+    
+    def get_mis_compras_view(self, request: Request):
+        return templates.TemplateResponse("mis_compras.html", {"request" : request})
+    
+    def get_producto_view(self, request: Request):
+        return templates.TemplateResponse("producto.html", {"request" : request})
+    
+    def get_recover_view(self, request: Request):
+        return templates.TemplateResponse("recover.html", {"request" : request})
+    
+    def get_register_view(self, request: Request):
+        return templates.TemplateResponse("register.html", {"request" : request}) 
+
+    def get_resumen_compra_view(self, request: Request):
+        return templates.TemplateResponse("resumen_compra.html", {"request" : request})  
+
+    def get_search_view(self, request: Request):
+        return templates.TemplateResponse("search.html", {"request" : request})
     
     def get_terminos_view(self, request: Request):
         return templates.TemplateResponse("terminos.html", {"request" : request})
     
-    def get_login_view(self, request: Request):
-        return templates.TemplateResponse("login.html", {"request" : request})
+    def get_tienda_view(self, request: Request):
+        return templates.TemplateResponse("tienda.html", {"request" : request})
     
-    def get_register_view(self, request: Request):
-        return templates.TemplateResponse("register.html", {"request" : request})
-    
-    def get_recover_view(self, request: Request):
-        return templates.TemplateResponse("recover.html", {"request" : request})
